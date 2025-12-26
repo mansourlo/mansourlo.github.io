7c9af585f05de843da1428db3bd76fcff0dae56e
mansourlo <mvnsour05@gmail.com>
2025-12-26 00:22:59 +0000

    responsive nav-bar done.

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 // ? afficher le contenu du menu au clic de l'icone de ce dernier
-const menuItems = document.getElementsByClassName("nav-bar")[0];
+const menuItems = document.getElementById("navig");
+const icone = document.getElementById("btn-menu");
 const showMenu = () => {
     menuItems.classList.toggle("menu");
+    icone.src = "images/btn-close.png";
+
+    if(menuItems.classList[1] !== "menu"){
+        icone.src = "images/btn-menu.png";
+    }
 }
